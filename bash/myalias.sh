05e648c3c1c5e8cc4be6160bbbae9333791fa9ed
Zeusro <692486802@qq.com>
2019-07-22 15:18:01 +0800

    Create myalias.sh

---
+++ bash/myalias.sh
@@ -0,0 +1,6 @@
+alias wget='wget -c '
+alias getpass="openssl rand -base64 20"
+alias sha='shasum -a 256 '
+alias ping='ping -c 5'
+alias speed='speedtest-cli --server 2406 --simple'
+alias ipi='ifconfig getifaddr en0'
